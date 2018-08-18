0180ea9c6cd6df7bf4d4707d5729e0daafdf7917
dom_linux <dom_linux@ddddd.xy>
2018-08-19 00:04:57 +0200

    kontroler ze sleep do testow hystrix

--- src/main/java/boot_eureka_client/PrzykladowyController.java
+++ src/main/java/boot_eureka_client/PrzykladowyController.java
@@ -10,4 +10,15 @@ public class PrzykladowyController {
 	public String mow(){
 		return "Cos mowie";
 	}
+	
+	@GetMapping("/mowDlugo")
+	public String mowDlugo(){
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "mowie dlugoooooo";
+	}
 }
